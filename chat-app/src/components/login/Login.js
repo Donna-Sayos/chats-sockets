5eaa4b59f027346e9ba592c34b655a21ef0629bf
Donna-Sayos <Donnasayos@gmail.com>
2022-09-28 19:05:37 -0400

    JSON fetch practice

--- chat-app/src/components/login/Login.js
+++ chat-app/src/components/login/Login.js
@@ -1,9 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./login.css";
 import { Container, Form } from "react-bootstrap";
 
 function Login() {
-    const idRef = useRef();
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+  const idRef = useRef();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
   return (
     <Container>
       <Form>
@@ -12,6 +21,17 @@ function Login() {
           <Form.Control type="text" ref={idRef} required />
         </Form.Group>
       </Form>
+      <br />
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      {items.map((item, i) => (
+        <div key={i}>
+            <p>{JSON.stringify(item)}</p>
+        </div>
+      ))}
     </Container>
   );
 }
