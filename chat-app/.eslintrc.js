a8113a520f690b40f0d4ce687ac8f15b91ded82c
Donna-Sayos <Donnasayos@gmail.com>
2022-10-05 12:07:20 -0400

    added babel preset

--- chat-app/.eslintrc.js
+++ chat-app/.eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
+  presets: ["@babel/preset-react"],
   env: {
     browser: true,
     node: true,
   },
-  "extends": ["@salesforce/eslint-config-lwc/recommended"] //this fixes the "Parsing error: The keyword 'import' is reserved"
+  extends: ["@salesforce/eslint-config-lwc/recommended"], //this fixes the "Parsing error: The keyword 'import' is reserved"
 };
--- chat-app/src/hooks/useLocalStorage.js
+++ chat-app/src/hooks/useLocalStorage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 // this file is so that the 'id' generated persists when the page refreshes or when we close our browser and come back;
 
-const PREFIX = "chat-app";
+const PREFIX = "chat-app"; // to organize data in localStorage; to know which info is from what app;
 
 function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key;
