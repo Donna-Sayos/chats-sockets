f008d9a0422f84867313564aedf17ad29f43d52b
Donna-Sayos <Donnasayos@gmail.com>
2022-10-06 22:20:42 -0400

    message showing but not done

--- chat-app/src/contexts/ConversationsProvider.js
+++ chat-app/src/contexts/ConversationsProvider.js
@@ -23,11 +23,31 @@ export function ConversationsProvider({ id, children }) {
   }
 
   function addMessageToConversation({ recipients, text, sender }) {
+    setConversations((prevConvo) => {
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newConversations = prevConvo.map((conversation) => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true;
+          return {
+            ...conversation,
+            messages: [...conversation.messages, newMessage],
+          };
+        }
 
+        return conversation;
+      });
+
+      if (madeChange) {
+        return newConversations;
+      } else {
+        return [...prevConvo, { recipients, messages: [newMessage] }];
+      }
+    });
   }
 
   function sendMessage(recipients, text) {
-    addMessageToConversation({ recipients, text, sender: id })
+    addMessageToConversation({ recipients, text, sender: id });
   }
 
   const formattedConversations = conversations.map((conversation, index) => {
@@ -38,8 +58,18 @@ export function ConversationsProvider({ id, children }) {
       const name = (contact && contact.name) || recipient;
       return { id: recipient, name };
     });
+
+    const messages = conversation.messages.map((message) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === message.sender;
+      });
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+
     const selected = index === selectedConversationIndex;
-    return { ...conversation, recipients, selected };
+    return { ...conversation, messages, recipients, selected };
   });
 
   const value = {
@@ -55,4 +85,15 @@ export function ConversationsProvider({ id, children }) {
       {children}
     </ConversationsContext.Provider>
   );
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
+
+  a.sort();
+  b.sort();
+
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }
